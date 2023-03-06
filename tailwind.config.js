1aa9ee0f45d09392f76a94d8f68b9ca82018e021
gabriel <gabrielcomputacao@outlook.com>
2023-03-06 08:04:43 -0300

    feat(header): criação do header da página

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    colors:{
-        'rosinha': 'E2B8AC',
-        'verdinho': '919F89',
-        'verdecinza': 'CDDAD8',
-        'cinzaescuro': '98A7AC',
+    extend: {
+       colors:{
+        'rosinha': '#E2B8AC',
+        'verdinho': '#919F89',
+        'verdecinza': '#CDDAD8',
+        'cinzaescuro': '#98A7AC',
     },
-    extend: {},
+    },
   },
   plugins: [],
 }
