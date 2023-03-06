4154e0daee2c65c0497b08f7cec2546497275e53
gabriel <gabrielcomputacao@outlook.com>
2023-03-06 06:57:23 -0300

    doc(configurando projeto): criando configuração para a página de psicologa

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors:{
+        'rosinha': 'E2B8AC',
+        'verdinho': '919F89',
+        'verdecinza': 'CDDAD8',
+        'cinzaescuro': '98A7AC',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
